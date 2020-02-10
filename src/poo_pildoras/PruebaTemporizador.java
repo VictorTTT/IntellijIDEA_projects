185891b710cb2ba6de4e257ba10139cc024fe6a7
VictorTTT <cotirena@gmail.com>
2020-02-10 13:16:08 +0100

    última cmprobación desde escritorio hacia portatil

---
+++ src/poo_pildoras/PruebaTemporizador.java
@@ -0,0 +1,75 @@
+package poo_pildoras;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import java.util.Date;
+
+import java.awt.Toolkit.*;
+
+public class PruebaTemporizador
+{
+    public static void main(String[] args)
+    {/*Vamos a crear un programa que muestre en consola información automaticamente cada cierto tiempo
+    para ello utilizaremos  la clase timer del paquete javax. swing. Esta clase tiene un constructor
+    al que debemos pasarle 2 parametros.
+    el primero de ellos se refiere al tiempo en milisegundos que va a tardar en aparecer dicho mensaje,
+     y el segundo es una interfaz*/
+
+        /*en la primera ampliación del programa , le añadimos la clase abstracta Toolkit para conseguir que cada vez que nos muestre la
+        hora también suene un sonido propio delSO. La clase toolkit es una especie de puente entre nuestro so y nuestros programas en java
+        (resolución de pantalla, sonido del sistema ,etc*/
+
+        //Debido a las reglas de herencia de las interfaces aquí también funcionaría:
+        //ActionListener oyente=new DameLaHora();
+        DameLaHora oyente=new DameLaHora();//creamos la instancia oyente de la clase DameLaHora
+        //que implementa la interfaz action listener
+
+
+        Timer mitemporizador=new Timer(5000,oyente);//(int delay, ////////Action listener////////)
+        /*para utilizar la interfaz action listener como parámetro, siguiendo las instrucciones
+         que nos marca la api
+        debemos : primero,importar el paquete al que coresponde(java.awt.event). Acto seguido, debido a la regla de
+        implementacion de metodos de las
+         interfaces debemos crear los mismos métodos en nuestra clase que los que tiene la propia
+         interfaz definidos en la api de java.
+         Siguiendo con la explicación, la interfaz action listener, implementa el método actionperformed(ActionEvent e)
+          que a su vez tiene un parámetro action event e. Si leemos en la definicion de este metodo veremos que nos dice
+           que es invocado cuando ocurre una determinada accion.
+
+           ¿Cómo vamos a solucionar esto?, pues creamos una clase que implemente la interfaz action lístener
+           en la cual crearemos un método actionPerformed(método de obligada creación por ser de una interfaz), después instanciaremos un objeto (oyente)
+           que será de tipo action listener y se lo pasaremos como parámetro a nuestro "mitemporizador"
+
+         */
+
+        mitemporizador.start();//le decimos al programa que comience a ejecutar la instrucción mi temporizador,
+        //para ello utilizamos el método start() perteneciente a la clase timer podemos usarlo gracias a que hemos importado paquete
+
+        JOptionPane.showMessageDialog(null,"Pulsa Aceptar para detener");
+        //debemos poner esta ventana debido a que de n o hacerlo el programa terminaria repentinamente
+        //Entonces gracias a que este tipo de acciones dejan el programa en ejecucion, podemos ver el funcionamiento del
+        // mismo durante el periodode tiempo que creamos necesario
+
+        System.exit(0);//esta insturucción sirve para que cuando aceptemos en el panel JOptionPane,
+        //el programa termine en la siguiente línea de codigo.
+
+    }
+}
+class DameLaHora implements ActionListener
+{/*Esto nos obliga a implementar a continuación todos los métodos que desarrolla esta interfaz, y allá vamos:*/
+
+    public void actionPerformed(ActionEvent actionEventObjetodeTipoEvento)
+    {//Como lo que queremos es que salga por pantalla cada cierto tiempo un mensaje con la hora, crearemos un
+        // objeto de la clase date:
+
+        Date ahora=new Date();//dentro de este objeto almacenamos la hora y se la pasaremos como parámetro al mensaje impreso en consola
+
+        System.out.printf("Te pongo la hora cada 5 seg: "+ ahora);
+
+        Toolkit.getDefaultToolkit().beep();//método de la clase toolkit que emite sonido cada vez que actua el evento
+
+    }
+}
---
+++ src/poo_pildoras/PruebaTemporizador2ClasesInternas.java
@@ -0,0 +1,93 @@
+package poo_pildoras;
+
+import javax.swing.*;
+
+import java.awt.event.*;
+
+import java.util.*;
+
+import javax.swing.Timer;
+
+import java.awt.Toolkit;
+
+public class PruebaTemporizador2ClasesInternas
+{
+    public static void main(String[] args)
+    {
+        Reloj miReloj=new Reloj(3000,true);
+        //instanciamos miReloj de la clase Reloj que al ser llamada por new
+        //nos pide 2 parámetros
+
+        miReloj.enMarcha();
+        //con el objeto recién creado llamamos al método que hemos creado para poner en funcionamiento el programa
+
+        JOptionPane.showMessageDialog(null,"pulsa aceptar para terminar");
+        //para que el programa se quede en ejecución sin finalizar
+
+        System.exit(0);
+        //Para que el programa finalice una vez pulsemos aceptar en la ventana
+    }
+}
+
+class Reloj
+{
+    public Reloj(int intervalo, boolean sonido)
+    //método constructor de la clase reloj que recibe 2 parámetros
+    //de forma que cada vez que instanciemos un objeto de esta clase,
+    // a la hora de invocar al constructor después de la palabra new,
+    // deberemos pasarle esos dos argumentos para crearlo.Por un lado el intervalo,
+    // con el que vamos a decidir cada cuánto tiempo vamos a imprimir en pantalla
+    //y por otro el sonido de tipo boolean que decidirá si pita o no.
+    {
+        this.intervalo=intervalo;
+
+        this.sonido=sonido;
+
+        //Desde el constructor damos estado inicial a los campos privados que hay en la clase Reloj
+        //con this. nos referimos al campo de clase que se llama igual que los argumentos que le pasamos al constructor
+    }
+
+    public void enMarcha()
+    //este método setter void porque no devuelve nada se encarga de poner en funcionamiento el programa
+    {
+       ActionListener oyente=new DameLaHora2();//DameLaHora2 es una clase interna
+        //instanciamos objeto oyente perteneciente a la interfaz action listener
+       Timer mitemporizador=new Timer(intervalo,oyente);
+       //instanciamos objeto de la clase timer que nos pide 2 argumentos
+        //por un lado el intervalo de tiempo con el que va actuar en milisegundos
+        //por otro un, objeto de tipo interfaz ActionListener (arriba declarado)
+
+        mitemporizador.start();////le decimos al programa que comience a ejecutar
+        // la instrucción mitemporizador,
+        //para ello utilizamos el método start() perteneciente a la clase timer
+
+    }
+
+    private int intervalo;//campo encapsulado de la clase Reloj
+
+    private boolean sonido;//campo encapsulado de la clase Reloj
+
+    private class DameLaHora2 implements ActionListener
+    //esto es una clase interna que lleva el modificador de acceso private, este modificador solamente pueden llevarlo estas clases
+    //además debemos implementar la interfaz ActionListener que a su vez nos obliga a crear los métodos que desarrolla e la API
+    {
+
+        public void actionPerformed(ActionEvent evento)
+        {
+            Date ahora=new Date();
+            //con este método de la clase date estamos almacenando la hora actual en el objeto ahora
+
+            System.out.printf("te pongo la hora cada 3 seg  "+ahora);
+            //damos formato a la salida por pantalla y concatenamos con con el objeto que contiene la hora
+
+            if (sonido)//si el usuario ha decidido que se emita un sonido la condición será cierta
+            {
+                Toolkit.getDefaultToolkit().beep();
+                //se reproduce sonido a través del método getDefaultToolkit().beep() de la clase Toolkit
+            }//en este if está la clave y el sentido de este programa, ya que estamos usando como condición un ejemplar
+            // de clase que es exterior a la clase en la que ahora nos encontramos. sonido está definido fuera de DameLaHora2
+            //esto podemos hacerlo gracias a que una clase interna puede acceder a lo que hay fuera de ella SIN NECESIDAD DE MÉTODO ALGUNO
+        }
+    }
+
+}

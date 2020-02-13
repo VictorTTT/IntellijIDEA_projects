188df58098a861ce701ca4dab5cfdd57bd4ffcd5
VictorTTT <cotirena@gmail.com>
2020-02-13 07:58:57 +0100

    Último commit desde el portatil

--- src/graficos/CreandoMarcos.java
+++ src/graficos/CreandoMarcos.java
@@ -1,6 +1,7 @@
 package graficos;
 
 import javax.swing.*;//para heredar de JFrame
+import java.awt.*;
 
 public class CreandoMarcos
 {
@@ -33,11 +34,30 @@ class miMarco extends JFrame//indicamos a la clase que creamos que queremos here
 {
     public miMarco()//método constructor de la clase que hemos creado (miMarco)
     {
-        setSize(500, 300);
+        //setSize(500, 300);
         /*Llamamos al método setSize de la clase Jframe. Le tenemos que pasar 2 enteros que se refieren
         al ancho y alto de la ventana gráfica que vamos a crear. Los parámetros pueden variar dependiendo del monitor
         dónde vayamos a mostrar el programa. Si tenemos problemas con este tamaño y quermos asegurarnos de cuál es
         el adecuado, podemos utilizar Toolkit para averiguar qué configuración es la adecuada para nuestro monitor*/
-        
+
+        //setLocation(500,300);/*Con este método vamos a especificar laubicación de la ventana. El método trabaja con
+        //un eje de coordenandas cartesiano y debemos pasarle 2 parámetros que se refieren al eje x y al y en px*/
+
+        setBounds(500,300,550,250);//con este método le damos tanto posición como tamaño, por lo que
+        // podemos prescindir de los dos anteriores cuando lo  utlizamos
+
+
+        /*setResizable(false);/*este método impide que la ventana gráfica resultante pueda ser modificada por el usuario
+         al introducir false. Si la dejáramos en vacía o en true el usuario podría redimensionarla*/
+
+        /*setExtendedState(Frame.MAXIMIZED_BOTH);/*este método utiliza unos estados como argumentos
+          campos de clase de la clase JFrame, existen 5 estados.Aquí utilizamos MAXIMIZED_BOTH
+          que nos permite redimensionar la ventana emergente a la totalidad de la pantalla. MAXIMIZED_BOTH es una constante de clase
+          estática,por lo tanto debemos usarla poniendo el nombre de la clase a a la que pertenece antes de ella Al ser
+          una constante de clase de tipo entero, tiene asignado un número, en este caso 6*/
+
+        setTitle("Mi ventana");//para darle nombre a ala ventana emergente
+
+
     }
 }
--- src/poo_pildoras/PruebaTemporizador.java
+++ src/poo_pildoras/PruebaTemporizador.java
@@ -24,12 +24,12 @@ public class PruebaTemporizador
 
         //Debido a las reglas de herencia de las interfaces aquí también funcionaría:
         //ActionListener oyente=new DameLaHora();
-        DameLaHora oyente=new DameLaHora();//creamos la instancia oyente de la clase DameLaHora
+        DameLaHora oyente = new DameLaHora();//creamos la instancia oyente de la clase DameLaHora
         //que implementa la interfaz action listener
 
 
-        Timer mitemporizador=new Timer(5000,oyente);//(int delay, ////////Action listener////////)
-        /*para utilizar la interfaz action listener como parámetro, siguiendo las instrucciones
+        Timer mitemporizador = new Timer(5000, oyente);//(int delay, ////////Action listener////////)
+        /*para utilizar la interfaz action listener; como parámetro, siguiendo las instrucciones
          que nos marca la api
         debemos : primero,importar el paquete al que coresponde(java.awt.event). Acto seguido, debido a la regla de
         implementacion de metodos de las
@@ -48,7 +48,7 @@ public class PruebaTemporizador
         mitemporizador.start();//le decimos al programa que comience a ejecutar la instrucción mi temporizador,
         //para ello utilizamos el método start() perteneciente a la clase timer podemos usarlo gracias a que hemos importado paquete
 
-        JOptionPane.showMessageDialog(null,"Pulsa Aceptar para detener");
+        JOptionPane.showMessageDialog(null, "Pulsa Aceptar para detener");
         //debemos poner esta ventana debido a que de n o hacerlo el programa terminaria repentinamente
         //Entonces gracias a que este tipo de acciones dejan el programa en ejecucion, podemos ver el funcionamiento del
         // mismo durante el periodode tiempo que creamos necesario
@@ -58,6 +58,7 @@ public class PruebaTemporizador
 
     }
 }
+
 class DameLaHora implements ActionListener
 {/*Esto nos obliga a implementar a continuación todos los métodos que desarrolla esta interfaz, y allá vamos:*/
 
@@ -65,9 +66,9 @@ class DameLaHora implements ActionListener
     {//Como lo que queremos es que salga por pantalla cada cierto tiempo un mensaje con la hora, crearemos un
         // objeto de la clase date:
 
-        Date ahora=new Date();//dentro de este objeto almacenamos la hora y se la pasaremos como parámetro al mensaje impreso en consola
+        Date ahora = new Date();//dentro de este objeto almacenamos la hora y se la pasaremos como parámetro al mensaje impreso en consola
 
-        System.out.println("Te pongo la hora cada 5 seg: "+ ahora);
+        System.out.println("Te pongo la hora cada 5 seg: " + ahora);
 
         Toolkit.getDefaultToolkit().beep();//método de la clase toolkit que emite sonido cada vez que actua el evento
 

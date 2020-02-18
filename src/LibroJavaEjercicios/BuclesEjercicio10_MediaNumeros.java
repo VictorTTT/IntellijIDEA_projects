6959534647d3a0ea068598a3a2c65ffe71f9dafc
VictorTTT <cotirena@gmail.com>
2020-02-18 17:37:01 +0100

    última cmprobación desde escritorio hacia portatil

---
+++ src/LibroJavaEjercicios/BuclesEjercicio10_MediaNumeros.java
@@ -0,0 +1,28 @@
+package LibroJavaEjercicios;
+
+//Escribe un programa que calcule la media de un conjunto de números positivos
+//introducidos por teclado. A priori, el programa no sabe cuántos números se
+//introducirán. El usuario indicará que ha terminado de introducir los datos
+//cuando meta un número negativo.
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class BuclesEjercicio10_MediaNumeros
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Introduce todos los números que quieras, cuando los tengas, " +
+                
+                " introduce uno negativo para terminar con la entrada y el programa continuará mostrando la media de todos ellos");
+        
+        
+            
+            
+            
+            
+        
+        
+        
+    }
+}

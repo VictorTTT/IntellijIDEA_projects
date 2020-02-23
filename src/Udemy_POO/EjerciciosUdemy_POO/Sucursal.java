e75735045da9fd84fbc6cd1e4df75defe1a59025
VictorTTT <cotirena@gmail.com>
2020-02-23 11:23:58 +0100

    última cmprobación desde escritorio hacia portatil

--- src/Udemy_POO/EjerciciosUdemy_POO/Sucursal.java
+++ src/Udemy_POO/EjerciciosUdemy_POO/Sucursal.java
@@ -3,49 +3,51 @@ package Udemy_POO.EjerciciosUdemy_POO;
 public class Sucursal
 {
     private String numeroSucursal;
-
+    
     private String direccion;
-
+    
     private String ciudad;
-
-
+    
+    
     public Sucursal(String numeroSucursal, String direccion, String ciudad)
     {
         this.numeroSucursal = numeroSucursal;
-
+        
         this.direccion = direccion;
-
+        
         this.ciudad = ciudad;
-
+        
     }
-
+    
     public String getNumeroSucursal()
     {
         return numeroSucursal;
     }
-
+    
     public String getDireccion()
     {
         return direccion;
     }
-
+    
     public String getCiudad()
     {
         return ciudad;
     }
-
-    public void calculaPrecio(Paquete miPaquete)
+    
+    public double calculaPrecio(Paquete miPaquete)
     {
-
-        double precio = miPaquete.getPeso() / 10;
-
+        
+        double precio = miPaquete.getPeso() / 100;
+        
         if (miPaquete.getPrioridad().equalsIgnoreCase("1"))
         {
-            precio+=10;
+            precio += 10;
         }
-        else if (miPaquete.getPrioridad().equalsIgnoreCase("2"))
+        if (miPaquete.getPrioridad().equalsIgnoreCase("2"))
         {
-            precio+=20;
+            precio += 20;
         }
+        
+        return precio;
     }
 }

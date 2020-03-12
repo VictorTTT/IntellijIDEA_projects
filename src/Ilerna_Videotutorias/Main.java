332d45e65796b10c08b9b41f842b12b842f35223
VictorTTT <cotirena@gmail.com>
2020-03-12 23:01:22 +0100

    Último commit desde el portatil

--- src/Ilerna_Videotutorias/Main.java
+++ src/Ilerna_Videotutorias/Main.java
@@ -1,16 +1,25 @@
 package Ilerna_Videotutorias;
 
 public class Main {
+
     public static void main(String[] args)//aprender de memoria
     {
         Puerta puerta1 = new Puerta();
+
+        puerta1.funcionPublica("Esta es una función pública");
+
+        puerta1.funcionPackage("Esta es una fución package");
+
+        puerta1.setAncho(101);
 
         Puerta puerta2 = new Puerta(200, 400);
 
         Puerta puerta3 = new Puerta(50, 50, "Rojo");
 
         System.out.println(puerta1.toString());
+
         System.out.println(puerta2.toString());
+
         System.out.println(puerta3.toString());
     }
 }
--- src/Ilerna_Videotutorias/Puerta.java
+++ src/Ilerna_Videotutorias/Puerta.java
@@ -1,15 +1,13 @@
 package Ilerna_Videotutorias;
 
-public class Puerta
-{
+public class Puerta {
     private int ancho = 0;
 
     private int alto = 0;
 
     private String color = "";
 
-    public Puerta()
-    {
+    public Puerta() {
         ancho = 150;
 
         alto = 200;
@@ -18,28 +16,65 @@ public class Puerta
     }
 
     public Puerta(int ancho, int alto)//por convención las variables de los constructores siempre se llaman igual
-           // que los campos de clase y deberemos utilizar la palabra this para diferenciarlos
+    // que los campos de clase y deberemos utilizar la palabra this para diferenciarlos
     {
-        color ="Blanco";
+        color = "Blanco";
 
-        this.ancho=ancho;
+        this.ancho = ancho;
 
-        this.alto=alto;
+        this.alto = alto;
     }
 
-    public Puerta(int ancho, int alto, String color)
-    {
-        this.ancho=ancho;
+    public Puerta(int ancho, int alto, String color) {
+        this.ancho = ancho;
 
-        this.alto=alto;
+        this.alto = alto;
 
-        this.color=color;
+        this.color = color;
     }
 
     public String toString()//Método para imprimir String en Java
     {
-        return "La puerta tiene las propiedades: Ancho: "+ancho+", Alto: "+alto
-                +", el Color: "+color;//luego lo devolvemos por si queremos hacer alguna otra cosa con ello
+
+        funcionPrivada("Función privada");//podemos acceder a ella gracias a que estamos en la misma clase, solo de esta forma porque es private
+
+        return "La puerta tiene las propiedades: Ancho: " + ancho + ", Alto: " + alto
+                + ", el Color: " + color;//luego lo devolvemos por si queremos hacer alguna otra cosa con ello
+
+
+    }
+
+    private void funcionPrivada(String texto) {
+
+        System.out.println(texto);
+    }
+
+    public void funcionPublica(String texto) {
+
+        System.out.println(texto);
+    }
+
+    void funcionPackage(String texto) {
+
+        System.out.println(texto);
+    }
+
+    public int getAncho() {
+
+        return ancho;
+    }
+
+    public void setAncho(int ancho) {
+
+        if (ancho < 100) {
+
+            System.out.println("El ancho debe ser mayor que 100");
+
+        } else {
+
+            this.ancho = ancho;
+
+        }
     }
 
 

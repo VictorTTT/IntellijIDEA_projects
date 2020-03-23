9606464f8a8acea1535876f03edd01a7bd538ce9
VictorTTT <cotirena@gmail.com>
2020-03-23 16:11:19 +0100

    Último commit desde el portatil

---
+++ src/Ilerna_Videotutorias/Alumno1.java
@@ -0,0 +1,39 @@
+package Ilerna_Videotutorias;
+
+import Udemy_POO.EjerciciosUdemy_POO.Alumno;
+
+public class Alumno1 extends Persona {
+
+    private String modulo;
+
+    public Alumno1(){
+        super();
+
+        modulo="m03b";
+    }
+
+    public Alumno1 (String nombre, int edad, String modulo){
+        super(nombre, edad);
+
+
+        this.modulo=modulo;
+
+
+
+    }
+
+    public String toString(){
+
+        return "Alumno con nombre"+getNombre()+" , edad "+ getEdad()+", módulo"+ modulo;
+    }
+
+    public void hablar( String texto){
+
+        System.out.println("Este es un método sobreescrito :"+texto);
+    }
+
+
+
+
+
+}

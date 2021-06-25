9384862fed96f833e4072ac7c69d6dcf9045f8f4
VictorTTT <cotirena@gmail.com>
2021-06-25 15:39:00 +0200

    Después de borrar el repositorio de prueba

--- src/LibroJava/POO/Explicaciones/Explicaciones2/EjemploArrayList01.java
+++ src/LibroJava/POO/Explicaciones/Explicaciones2/EjemploArrayList01.java
@@ -5,7 +5,21 @@ import java.util.ArrayList;
 public class EjemploArrayList01 {
     public static void main(String[] args) {
 
+
         ArrayList<String> a = new ArrayList<>();
+
+        System.out.println("Número de elementos " + a.size());
+
+        a.add("rojo");
+        a.add("verde");
+        a.add("azul");
+
+        System.out.println("Número de elementos " + a.size());
+
+        a.add("Blanco");
+
+        System.out.println("Número de elementos " + a.size());
+
 
     }
 }

856a5c230690ebd5ce0788b92fe2c750fbcd0021
VictorTTT <cotirena@gmail.com>
2021-06-25 15:34:24 +0200

    Después de borrar el repositorio de prueba

--- src/LibroJava/POO/Explicaciones/Explicaciones2/EjemploArrayList01.java
+++ src/LibroJava/POO/Explicaciones/Explicaciones2/EjemploArrayList01.java
@@ -1,7 +1,11 @@
 package LibroJava.POO.Explicaciones.Explicaciones2;
 
+import java.util.ArrayList;
+
 public class EjemploArrayList01 {
     public static void main(String[] args) {
+
+        ArrayList<String> a = new ArrayList<>();
 
     }
 }
